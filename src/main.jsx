b2d6f5a8ac77e2dd5d7c6f0a1fb0baa4662f203c
Shubham Maurya <smjnv89@gmail.com>
2025-01-19 10:35:37 +0530

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthContext } from '../context/authContext'
+import AuthContext from './context/authContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <AuthContext>
