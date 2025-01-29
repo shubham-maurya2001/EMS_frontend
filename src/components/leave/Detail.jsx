201f4c23d3fd4597792a2b236b63eb630b0bfa68
Shubham Maurya <smjnv89@gmail.com>
2025-01-29 09:26:58 +0530

    Detail.jsx

--- src/components/leave/Detail.jsx
+++ src/components/leave/Detail.jsx
@@ -48,7 +48,7 @@ const Detail = () => {
       <div className="max-w-3xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md">
         <h2 className="text-2xl font-bold mb-8 text-center">Leave Details</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div>
+          <div className='flex justify-center'>
             <img className="rounded-full border w-72" src={`${leaves.employeeId.userId.profileImageUrl}`} alt="" />
           </div>
           <div>
